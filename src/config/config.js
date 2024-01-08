9e255a563d8b65dc0b625e3e4fd4b6f75110f6b8
Kartik Thakur <thakurkartik2262@gmail.com>
2024-01-08 10:39:39 +0530

    fix dot env error

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 let baseURL;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.REACT_APP_NODE_ENV === "production") {
   baseURL = process.env.REACT_APP_BASE_URL;
 } else {
-  baseURL = 'http://localhost:3000'; // Development URL
+  baseURL = "http://localhost:3000"; // Development URL
 }
 
-export default baseURL
+export default baseURL;
